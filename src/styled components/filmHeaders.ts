385a3a6d0c30bb54a7ee2a2bf0234c286ef85a3a
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-08-21 19:22:05 +0200

    Styled conponent for Banner headers

---
+++ src/styled components/filmHeaders.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const BannerHeader = styled.h2`
+  color: antiquewhite;
+  font-family: Arial, Helvetica, sans-serif;
+  background-color: #0d1321;
+  padding-top: 0.7rem;
+  padding-left: 1.2rem;
+  font-size: 2rem;
+`
