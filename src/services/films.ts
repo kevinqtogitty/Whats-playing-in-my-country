f2561212d58fbef3c803d905ace99448441b9e99
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-08-21 19:24:55 +0200

    Axios HTTP requests to TMDB database

---
+++ src/services/films.ts
@@ -0,0 +1,21 @@
+import { currentBaseUrl, upcomingMoviesBaseUrl } from '../constants/constants'
+
+import axios from 'axios'
+import { Films } from '../types/film'
+
+export const currentlyPlaying = async (
+  country: String,
+  setFilms: React.Dispatch<React.SetStateAction<Films[] | undefined>>,
+) => {
+  const { data } = await axios.get(`${currentBaseUrl}${country}`)
+  setFilms(data.results)
+}
+
+export const upcomingMovies = async (
+  country: String,
+  setUpcomingMovies: React.Dispatch<React.SetStateAction<Films[] | undefined>>,
+) => {
+  const { data } = await axios.get(`${upcomingMoviesBaseUrl}${country}`)
+
+  setUpcomingMovies(data.results)
+}
