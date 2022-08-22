57c1a3f9e04d203807fbac40dcdea418dbad6d9a
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-08-22 11:09:29 +0200

    Comments

--- src/styled components/styled components combined/ChooseCountryDropdown.tsx
+++ src/styled components/styled components combined/ChooseCountryDropdown.tsx
@@ -1,23 +1,31 @@
-import React, { useState, useEffect, SetStateAction } from 'react'
+import React, { useState, useEffect } from 'react'
 
+//Individual Components
 import { CountryDropdown } from '../countryDropdown'
 import { CountryOption } from '../buttons'
 
+//Constant Variables
 import { countryOptions } from '../../constants/constants'
 
+//Services
 import { currentlyPlaying, upcomingMovies } from '../../services/films'
+
+//Types
 import { Films } from '../../types/film'
 
 interface ChooseCountryProps {
+  //Setter functions get this type definition
   setFilms: React.Dispatch<React.SetStateAction<Films[]>>
   setUpcomingFilms: React.Dispatch<React.SetStateAction<Films[]>>
 }
 
-const ChooseCountry: React.FC<ChooseCountryProps> = (props: ChooseCountryProps) => {
+const ChooseCountry: React.FC<ChooseCountryProps> = (props) => {
   const { setFilms, setUpcomingFilms } = props
   const [countryKey, setCountryKey] = useState<String>('GB')
   const [currentCountry, setCurrentCountry] = useState<String>('United Kingdom')
 
+  //Onchange prop type is the ChangeEvent (pressing keyboard, choosing select option etc),
+  //on a specific element, the HTMLSelectElement
   const changeCountry = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const switchCountryKey = event.target.value
     const countryToSwitch = countryOptions.find((country) => country.id === switchCountryKey)
