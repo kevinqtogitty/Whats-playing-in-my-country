f1057cff71d70e1ff72b650ea5d487ae5f3a64d6
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-08-22 19:49:28 +0200

    -

--- src/styled components/styled components combined/WhatsPlayingIn.tsx
+++ src/styled components/styled components combined/WhatsPlayingIn.tsx
@@ -2,21 +2,27 @@ import { posterBaseUrl } from '../../constants/constants'
 import { FilmsBanner } from '../filmSideScrollingBanner'
 import { FilmPosters } from '../filmPosters'
 import { Films } from '../../types/film'
-import React from 'react'
+import React, { useContext } from 'react'
+import { BannerHeader } from '../filmHeaders'
+import { CurrentCountryContext } from '../../context'
 
 interface Props {
   films: Films[]
 }
 
 const WhatsPlayingIn: React.FC<Props> = ({ films }) => {
+  const { currentCountry } = useContext(CurrentCountryContext)
   return (
-    <FilmsBanner>
-      {films?.map((film) =>
-        film.poster_path !== null ? (
-          <FilmPosters src={`${posterBaseUrl}${film.poster_path}`} />
-        ) : null,
-      )}
-    </FilmsBanner>
+    <>
+      <BannerHeader>What's playing in {currentCountry}</BannerHeader>
+      <FilmsBanner>
+        {films?.map((film) =>
+          film.poster_path !== null ? (
+            <FilmPosters src={`${posterBaseUrl}${film.poster_path}`} />
+          ) : null,
+        )}
+      </FilmsBanner>
+    </>
   )
 }
 
--- src/types/countries.ts
+++ src/types/countries.ts
@@ -1,4 +1,4 @@
 export interface Countries {
-  label: String
+  label: string
   id: string
 }
