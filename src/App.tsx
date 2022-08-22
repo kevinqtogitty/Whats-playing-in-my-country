14840442261bbf7033e42ba2614e1efd5f308435
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-08-22 11:10:11 +0200

    Now Playing & Upcoming films have been refactored

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,26 @@
 //Tools
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { posterBaseUrl } from './constants/constants'
 
 //types
 import { Films } from './types/film'
 
-//Services
-import { currentlyPlaying } from './services/films'
-import { upcomingMovies } from './services/films'
-
 //Styled Components
-import { Body } from './styled components/body'
-import { FilmPosters } from './styled components/filmPosters'
+import ChooseCountry from './styled components/styled components combined/ChooseCountryDropdown'
+import WhatsPlayingIn from './styled components/styled components combined/WhatsPlayingIn'
 import { FilmsBanner } from './styled components/filmSideScrollingBanner'
 import { BannerHeader } from './styled components/filmHeaders'
-import { CountryDropdown } from './styled components/countryDropdown'
-import { CountryOption } from './styled components/buttons'
+import { FilmPosters } from './styled components/filmPosters'
+import { Body } from './styled components/body'
 
 //Constants
-import { countryOptions } from './constants/constants'
 import { NavBar } from './styled components/navBar'
-import ChooseCountry from './styled components/styled components combined/ChooseCountryDropdown'
+import UpcomingIn from './styled components/styled components combined/UpcomingIn'
 
 const App: React.FC = () => {
-  const [films, setFilms] = useState<Films[]>([])
-  const [upcomingFilms, setUpcomingFilms] = useState<Films[]>([])
-  const [countryKey, setCountryKey] = useState<String>('GB')
   const [currentCountry, setCurrentCountry] = useState<String>('United Kingdom')
+  const [upcomingFilms, setUpcomingFilms] = useState<Films[]>([])
+  const [films, setFilms] = useState<Films[]>([])
 
   return (
     <Body>
@@ -36,21 +30,9 @@ const App: React.FC = () => {
       Country
       <br />
       <BannerHeader>What's playing in {currentCountry}</BannerHeader>
-      <FilmsBanner>
-        {films?.map((film) =>
-          film.poster_path !== null ? (
-            <FilmPosters src={`${posterBaseUrl}${film.poster_path}`} />
-          ) : null,
-        )}
-      </FilmsBanner>
+      <WhatsPlayingIn films={films} />
       <BannerHeader>Upcoming films in {currentCountry}</BannerHeader>
-      <FilmsBanner>
-        {upcomingFilms?.map((film) =>
-          film.poster_path !== null ? (
-            <FilmPosters src={`${posterBaseUrl}${film.poster_path}`} />
-          ) : null,
-        )}
-      </FilmsBanner>
+      <UpcomingIn upcomingFilms={upcomingFilms} />
     </Body>
   )
 }
