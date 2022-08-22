6f0f085d05cc187aa79486506269dfaf52978800
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-08-22 19:51:02 +0200

    Whats Playing and upcoming have been refactored into their own components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 //Tools
 import React, { useState } from 'react'
-import { posterBaseUrl } from './constants/constants'
 
 //types
 import { Films } from './types/film'
@@ -8,31 +7,29 @@ import { Films } from './types/film'
 //Styled Components
 import ChooseCountry from './styled components/styled components combined/ChooseCountryDropdown'
 import WhatsPlayingIn from './styled components/styled components combined/WhatsPlayingIn'
-import { FilmsBanner } from './styled components/filmSideScrollingBanner'
-import { BannerHeader } from './styled components/filmHeaders'
-import { FilmPosters } from './styled components/filmPosters'
 import { Body } from './styled components/body'
 
 //Constants
 import { NavBar } from './styled components/navBar'
 import UpcomingIn from './styled components/styled components combined/UpcomingIn'
+import { CurrentCountryContext } from './context'
 
 const App: React.FC = () => {
-  const [currentCountry, setCurrentCountry] = useState<String>('United Kingdom')
+  const [currentCountry, setCurrentCountry] = useState<string>('United Kingdom')
   const [upcomingFilms, setUpcomingFilms] = useState<Films[]>([])
   const [films, setFilms] = useState<Films[]>([])
 
   return (
     <Body>
-      <NavBar>
-        <ChooseCountry setFilms={setFilms} setUpcomingFilms={setUpcomingFilms} />
-      </NavBar>
-      Country
-      <br />
-      <BannerHeader>What's playing in {currentCountry}</BannerHeader>
-      <WhatsPlayingIn films={films} />
-      <BannerHeader>Upcoming films in {currentCountry}</BannerHeader>
-      <UpcomingIn upcomingFilms={upcomingFilms} />
+      <CurrentCountryContext.Provider value={{ currentCountry, setCurrentCountry }}>
+        <NavBar>
+          <ChooseCountry setFilms={setFilms} setUpcomingFilms={setUpcomingFilms} />
+        </NavBar>
+        Country
+        <br />
+        <WhatsPlayingIn films={films} />
+        <UpcomingIn upcomingFilms={upcomingFilms} />
+      </CurrentCountryContext.Provider>
     </Body>
   )
 }
