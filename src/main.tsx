899aa67b38279182e0a0296f71fadbb7d5934b13
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-08-23 11:14:27 +0200

    Added React Router v6

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React from 'react'
+import App from './pages/App'
+import LoginPage from './pages/Login'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='login' element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
