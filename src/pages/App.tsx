e433af60cdf232faf2b3c67c562e085ceea95ab2
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-08-23 11:12:41 +0200

    Moved file

---
+++ src/pages/App.tsx
@@ -0,0 +1,34 @@
+//Tools
+import React, { useState } from 'react'
+
+//types
+import { Films } from '../types/film'
+
+//Styled Components
+import ChooseCountry from '../functional components/ChooseCountryDropdown'
+import WhatsPlayingIn from '../functional components/WhatsPlayingIn'
+import { Body } from '../styled components/body'
+
+//Constants
+import UpcomingIn from '../functional components/UpcomingIn'
+import { CurrentCountryContext } from '../context'
+import NavBar from '../functional components/NavBar'
+
+const App: React.FC = () => {
+  const [currentCountry, setCurrentCountry] = useState<string>('United Kingdom')
+  const [upcomingFilms, setUpcomingFilms] = useState<Films[]>([])
+  const [films, setFilms] = useState<Films[]>([])
+
+  return (
+    <Body>
+      <CurrentCountryContext.Provider value={{ currentCountry, setCurrentCountry }}>
+        <NavBar />
+        <ChooseCountry setFilms={setFilms} setUpcomingFilms={setUpcomingFilms} />
+        <WhatsPlayingIn films={films} />
+        <UpcomingIn upcomingFilms={upcomingFilms} />
+      </CurrentCountryContext.Provider>
+    </Body>
+  )
+}
+
+export default App
