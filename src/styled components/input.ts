9f378a29ae9c7da0551d9016644c235bb93c3b98
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-08-23 11:10:56 +0200

    -

---
+++ src/styled components/input.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const Input = styled.input`
+  display: block;
+  min-width: 90%;
+  margin: 1em;
+  padding: 1em;
+  width: 35em;
+  border-radius: 8px;
+  border-style: none;
+  border: 1px solid #e4e6e8;
+  transition: 0.1s ease;
+
+  &:hover {
+    border-color: #88d6f2;
+  }
+`
