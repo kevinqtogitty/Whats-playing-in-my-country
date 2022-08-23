7d90f077a6c4d3008f4aa36ba5a45f5a58a88e9a
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-08-23 11:12:19 +0200

    Initial commit

---
+++ src/pages/Login.tsx
@@ -0,0 +1,13 @@
+import LoginForm from '../functional components/LoginForm'
+import NavBar from '../functional components/NavBar'
+
+const LoginPage = () => {
+  return (
+    <>
+      <NavBar></NavBar>
+      <LoginForm />
+    </>
+  )
+}
+
+export default LoginPage
