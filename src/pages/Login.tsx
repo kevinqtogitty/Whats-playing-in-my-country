726b0ea48613adf537e50dc99fa9f619c7a3e596
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-08-23 15:42:05 +0200

    refactored login and signup form into seperate components

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,37 @@
+import React, { useState } from 'react'
+import { UserCredentials } from '../context'
 import LoginForm from '../functional components/LoginForm'
 import NavBar from '../functional components/NavBar'
+import SignUpForm from '../functional components/SignUpForm'
 
 const LoginPage = () => {
+  const [firstName, setFirstName] = useState<string>('')
+  const [lastName, setLastName] = useState<string>('')
+  const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+  const [signUpOrLogin, setFormType] = useState<boolean>(true)
+
+  const whichForm = signUpOrLogin === true ? <LoginForm /> : <SignUpForm />
+
   return (
     <>
       <NavBar></NavBar>
-      <LoginForm />
+      <UserCredentials.Provider
+        value={{
+          firstName,
+          setFirstName,
+          lastName,
+          setLastName,
+          email,
+          setEmail,
+          password,
+          setPassword,
+          signUpOrLogin,
+          setFormType,
+        }}
+      >
+        {whichForm}
+      </UserCredentials.Provider>
     </>
   )
 }
