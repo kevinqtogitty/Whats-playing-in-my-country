1e6e844950e3327ad41c176d78ffd6b55c996729
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-08-23 11:10:28 +0200

    Added Styles for UL and LI

--- src/styled components/navBar.ts
+++ src/styled components/navBar.ts
@@ -1,6 +1,17 @@
 import styled from 'styled-components'
 
-export const NavBar = styled.nav`
+export const Navigation = styled.nav`
   display: flex;
-  justify-content: flex-end;
+  justify-content: flex-start;
+  border: 1rem, solid, black;
+`
+
+export const UlNavList = styled.ul`
+  display: flex;
+  column-gap: 1rem;
+`
+
+export const LiNavList = styled.li`
+  text-decoration: none;
+  list-style: none;
 `
