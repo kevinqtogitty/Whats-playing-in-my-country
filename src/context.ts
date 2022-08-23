7d518c3fa7434572d9885e208c059bda187d04a8
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-08-23 15:43:58 +0200

    Added store for User authentication state variables

--- src/context.ts
+++ src/context.ts
@@ -1,7 +1,23 @@
 import React, { createContext } from 'react'
 
-interface Store {
+interface CurrentCountryStore {
   currentCountry: string
   setCurrentCountry: React.Dispatch<React.SetStateAction<string>>
 }
-export const CurrentCountryContext = createContext<Store>({} as Store)
+export const CurrentCountryContext = createContext<CurrentCountryStore>({} as CurrentCountryStore)
+
+interface UserCredentials {
+  firstName: string
+  lastName: string
+  email: string
+  password: string
+  signUpOrLogin: boolean
+
+  setFirstName: React.Dispatch<React.SetStateAction<string>>
+  setLastName: React.Dispatch<React.SetStateAction<string>>
+  setEmail: React.Dispatch<React.SetStateAction<string>>
+  setPassword: React.Dispatch<React.SetStateAction<string>>
+  setFormType: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+export const UserCredentials = createContext<UserCredentials>({} as UserCredentials)
