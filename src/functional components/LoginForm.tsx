d2f6aa7edc6a45819855cde48cf6bd6b9032235e
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-08-23 15:43:29 +0200

    -

--- src/functional components/LoginForm.tsx
+++ src/functional components/LoginForm.tsx
@@ -1,68 +1,25 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
+import { UserCredentials } from '../context'
 import { Button } from '../styled components/buttons'
 import { Input } from '../styled components/input'
 
 const LoginForm: React.FC = () => {
-  const [firstName, setFirstName] = useState<string>('')
-  const [lastName, setLastName] = useState<string>('')
-  const [email, setEmail] = useState<string>('')
-  const [password, setPassword] = useState<string>('')
-  const [signUpOrLogin, setFormType] = useState<boolean>(true)
+  const { email, setEmail } = useContext(UserCredentials)
+  const { password, setPassword } = useContext(UserCredentials)
+  const { signUpOrLogin, setFormType } = useContext(UserCredentials)
 
-  const handleFormSwitch = () => {
-    setFormType(!signUpOrLogin)
-  }
-
-  if (signUpOrLogin) {
-    return (
-      <>
-        <form action='submit'>
-          <Input
-            value={email}
-            type='text'
-            placeholder='Email'
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <Input
-            value={password}
-            type='password'
-            placeholder='Password'
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <Button type='submit'>Login</Button> <br />
-        </form>
-        <Button onClick={handleFormSwitch}>Sign Up</Button>
-        <p>Forgot password?</p>
-      </>
-    )
-  }
+  const handleFormSwitch = () => setFormType(!signUpOrLogin)
 
   return (
     <>
       <form action='submit'>
         <Input
-          name='firstName'
-          value={firstName}
-          type='text'
-          placeholder='First Name'
-          onChange={(e) => setFirstName(e.target.value)}
-        />
-        <Input
-          name='lastName'
-          value={lastName}
-          type='text'
-          placeholder='Surname'
-          onChange={(e) => setLastName(e.target.value)}
-        />
-        <Input
-          name='email'
           value={email}
           type='text'
           placeholder='Email'
           onChange={(e) => setEmail(e.target.value)}
         />
         <Input
-          name='password'
           value={password}
           type='password'
           placeholder='Password'
@@ -70,7 +27,7 @@ const LoginForm: React.FC = () => {
         />
         <Button type='submit'>Login</Button> <br />
       </form>
-      <Button onClick={handleFormSwitch}>Login</Button>
+      <Button onClick={handleFormSwitch}>Sign Up</Button>
       <p>Forgot password?</p>
     </>
   )
