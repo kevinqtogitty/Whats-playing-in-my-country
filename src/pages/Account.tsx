78cc7b3e31e812996d77c200839b69d9c3eba0a9
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-08-24 09:51:58 +0200

    Initial

---
+++ src/pages/Account.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from '../functional components/NavBar'
+
+const Account = () => {
+  return (
+    <>
+      <NavBar></NavBar>
+      <div>Account</div>
+    </>
+  )
+}
+
+export default Account
