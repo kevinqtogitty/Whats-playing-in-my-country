9d0fd2b1a417abf1cd4b825fe0ae3b0be99d5e76
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-08-24 19:52:43 +0200

    Added signout button/functionality

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
 import NavBar from '../functional components/NavBar'
+import { getAuth, signOut } from 'firebase/auth'
+import { Button } from '../styled components/buttons'
 
 const Account = () => {
+  const auth = getAuth()
   return (
     <>
       <NavBar></NavBar>
       <div>Account</div>
+      <Button onClick={() => signOut(auth)}>Sign Out</Button>
     </>
   )
 }
