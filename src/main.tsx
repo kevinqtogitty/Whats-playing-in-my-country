ee61debab2d59afca9d6566091f189679f943a83
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-08-24 09:52:37 +0200

    Updated routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,18 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import React from 'react'
 import App from './pages/App'
-import LoginPage from './pages/Login'
+import Account from './pages/Account'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<App />} />
-        <Route path='login' element={<LoginPage />} />
+        <Route path='signIn' element={<SignIn />} />
+        <Route path='signUp' element={<SignUp />} />
+        <Route path='account' element={<Account />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
