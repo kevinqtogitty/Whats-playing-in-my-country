97d52b4e739859502ae6ef8d0f21eba65c8262cc
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-08-24 19:53:52 +0200

    -

---
+++ src/contexts/userAuth.tsx
@@ -0,0 +1,34 @@
+import { getAuth, onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+export interface AuthRouteProps {
+  children: React.ReactNode
+}
+
+const AuthRoute: React.FC<AuthRouteProps> = (props) => {
+  const { children } = props
+  const auth = getAuth()
+  const navigate = useNavigate()
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    AuthCheck()
+    return () => AuthCheck()
+  }, [auth])
+
+  const AuthCheck = onAuthStateChanged(auth, (user) => {
+    if (user) {
+      setLoading(false)
+    } else {
+      console.log('Unauthorized')
+      navigate('/signIn')
+    }
+  })
+
+  if (loading) return <p>Loading...</p>
+
+  return <>{children}</>
+}
+
+export default AuthRoute
