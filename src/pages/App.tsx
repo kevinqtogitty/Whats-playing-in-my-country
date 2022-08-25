608451515149ba6b89186cd6ca7df903447290a1
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-08-25 11:30:59 +0200

    -

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -5,29 +5,39 @@ import React, { useState } from 'react'
 import { Films } from '../types/film'
 
 //Styled Components
-import ChooseCountry from '../functional components/ChooseCountryDropdown'
 import WhatsPlayingIn from '../functional components/WhatsPlayingIn'
-import { Body } from '../styled components/body'
 
 //Constants
 import UpcomingIn from '../functional components/UpcomingIn'
 import { CurrentCountryContext } from '../contexts/context'
 import NavBar from '../functional components/NavBar'
 
+import styled from 'styled-components'
+
 const App: React.FC = () => {
   const [currentCountry, setCurrentCountry] = useState<string>('United Kingdom')
   const [upcomingFilms, setUpcomingFilms] = useState<Films[]>([])
   const [films, setFilms] = useState<Films[]>([])
 
+  const Body = styled.body`
+    margin-left: 0px;
+  `
+
   return (
-    <>
-      <CurrentCountryContext.Provider value={{ currentCountry, setCurrentCountry }}>
-        <NavBar />
-        <ChooseCountry setFilms={setFilms} setUpcomingFilms={setUpcomingFilms} />
-        <WhatsPlayingIn films={films} />
-        <UpcomingIn upcomingFilms={upcomingFilms} />
-      </CurrentCountryContext.Provider>
-    </>
+    <CurrentCountryContext.Provider
+      value={{
+        currentCountry,
+        setCurrentCountry,
+        films,
+        setFilms,
+        upcomingFilms,
+        setUpcomingFilms,
+      }}
+    >
+      <NavBar />
+      <WhatsPlayingIn films={films} />
+      <UpcomingIn upcomingFilms={upcomingFilms} />
+    </CurrentCountryContext.Provider>
   )
 }
 
