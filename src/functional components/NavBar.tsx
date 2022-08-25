4b3699c89d6372a23a75c340c65914e964dcd77a
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-08-25 11:32:59 +0200

    moved styled components into the same file

--- src/functional components/NavBar.tsx
+++ src/functional components/NavBar.tsx
@@ -1,16 +1,38 @@
-import { LiNavList, Navigation, UlNavList } from '../styled components/navBar'
 import { Link } from 'react-router-dom'
+import ChooseCountry from './ChooseCountryDropdown'
+import styled from 'styled-components'
+
+const Navigation = styled.nav`
+  display: flex;
+  justify-content: flex-start;
+  border: 1rem, solid, black;
+  margin: 0px;
+`
+
+const UlNavList = styled.ul`
+  display: flex;
+  column-gap: 1rem;
+  text-decoration: none;
+`
+
+const LiNavList = styled.li`
+  text-decoration: none;
+  list-style: none;
+  list-style-type: none;
+  color: black;
+`
 
 const NavBar = () => {
   return (
     <Navigation>
       <UlNavList>
-        <LiNavList>
-          <Link to='/'>Home</Link>
-        </LiNavList>
-        <LiNavList>
-          <Link to='/signIn'>Sign In</Link>
-        </LiNavList>
+        <Link to='/' style={{ textDecoration: 'none' }}>
+          <LiNavList>Home</LiNavList>
+        </Link>
+        <Link to='/signIn' style={{ textDecoration: 'none' }}>
+          <LiNavList>Sign In</LiNavList>
+        </Link>
+        <ChooseCountry />
       </UlNavList>
     </Navigation>
   )
