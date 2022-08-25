da81ec1f9a867b9977cacd4f4c48ee33e930a67a
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-08-25 11:29:53 +0200

    -

--- src/styled components/filmPosters.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-
-export const FilmPosters = styled.img`
-  border-radius: 1rem;
-  &:hover {
-    opacity: 50%;
-  }
-`
--- src/styled components/formWrapper.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-
-export const FormWrapper = styled.div`
-  margin-top: 6rem;
-`
-export const FlexWrapper = styled.div`
-  display: flex;
-`
