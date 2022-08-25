496ede4f6981d8744e279e56bcdb44f7925ef75b
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-08-25 11:31:45 +0200

    -

---
+++ src/functional components/styled components/filmSideScrollingBanner.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const FilmsBanner = styled.div`
+  display: flex;
+  column-gap: 1rem;
+  background-color: #0d1321;
+  width: auto;
+  overflow: scroll;
+  padding: 1rem;
+  margin-top: -1.7rem;
+  margin: 0px;
+`
