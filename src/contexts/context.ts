92c0b4d4d3392e6684253fe7eb97df89607c2b2d
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-08-25 11:34:24 +0200

    -

--- src/contexts/context.ts
+++ src/contexts/context.ts
@@ -1,8 +1,13 @@
 import React, { createContext } from 'react'
+import { Films } from '../types/film'
 
 interface CurrentCountryStore {
   currentCountry: string
   setCurrentCountry: React.Dispatch<React.SetStateAction<string>>
+  upcomingFilms: Films[]
+  setUpcomingFilms: React.Dispatch<React.SetStateAction<Films[]>>
+  films: Films[]
+  setFilms: React.Dispatch<React.SetStateAction<Films[]>>
 }
 export const CurrentCountryContext = createContext<CurrentCountryStore>({} as CurrentCountryStore)
 
