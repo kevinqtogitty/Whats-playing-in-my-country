70c8c4584af4d3516cad67cf31e8660ef7f76a69
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-08-25 11:32:11 +0200

    -

---
+++ src/functional components/styled components/body.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const FormBody = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+export const MainBody = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background-color: #e6d893;
+`
