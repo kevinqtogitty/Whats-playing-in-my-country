9781de8f3e5e46b4bd28615f66b75c1586868f0d
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-08-26 11:08:34 +0200

    Added signedInOrNot variable into context

--- src/contexts/context.ts
+++ src/contexts/context.ts
@@ -2,21 +2,16 @@ import React, { createContext } from 'react'
 import { Films } from '../types/film'
 
 interface CurrentCountryStore {
+  currentCountryKey: string
+  setCurrentCountryKey: React.Dispatch<React.SetStateAction<string>>
   currentCountry: string
   setCurrentCountry: React.Dispatch<React.SetStateAction<string>>
   upcomingFilms: Films[]
   setUpcomingFilms: React.Dispatch<React.SetStateAction<Films[]>>
   films: Films[]
   setFilms: React.Dispatch<React.SetStateAction<Films[]>>
+
+  signedInOrNot: boolean
+  setSignedInOrNot: React.Dispatch<React.SetStateAction<boolean>>
 }
 export const CurrentCountryContext = createContext<CurrentCountryStore>({} as CurrentCountryStore)
-
-interface UserCredentials {
-  email: string
-  password: string
-
-  setEmail: React.Dispatch<React.SetStateAction<string>>
-  setPassword: React.Dispatch<React.SetStateAction<string>>
-}
-
-export const UserCredentials = createContext<UserCredentials>({} as UserCredentials)
