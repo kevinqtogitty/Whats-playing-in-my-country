f9fd0ae091c3ae2ab46f0a76ff631549c2718896
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-08-26 11:09:42 +0200

    -

--- src/main.tsx
+++ src/main.tsx
@@ -1,32 +1,14 @@
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import React from 'react'
 import App from './pages/App'
-import Account from './pages/Account'
-import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
+
 import { initializeApp } from 'firebase/app'
 import { config } from './firebase'
-import AuthRoute from './contexts/userAuth'
 
 initializeApp(config.firebase)
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<App />} />
-        <Route path='signIn' element={<SignIn />} />
-        <Route path='signUp' element={<SignUp />} />
-        <Route
-          path='account'
-          element={
-            <AuthRoute>
-              <Account />{' '}
-            </AuthRoute>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
 )
