18fffac7d03c13022565999db4542b83aafaf573
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-08-27 23:20:34 +0200

    -

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -1,7 +1,25 @@
+import { doc, getDoc } from 'firebase/firestore'
+import { useContext } from 'react'
+import { CurrentCountryContext } from '../contexts/context'
+import WatchlistCards from '../functional components/WatchlistCards'
+import { firestoreDB } from '../main'
+
 const Account = () => {
+  // const { userWatchList, setUserWatchList, signedInOrNot, currentUID } =
+  //   useContext(CurrentCountryContext)
+  //if user is logged in
+  //Then take the current UID and retrieve every film from the watchlist collection
+  //Then populate the usersWatchlist
+  //map out the cards to display the current users watchlist
+  // if (userDoc) {
+  //   console.log(userDoc)
+  // } else {
+  //   console.log('nothing found')
+  // }
   return (
     <>
-      <div>Account</div>
+      <h2>Whats on your watchlist!</h2>
+      <WatchlistCards></WatchlistCards>
     </>
   )
 }
