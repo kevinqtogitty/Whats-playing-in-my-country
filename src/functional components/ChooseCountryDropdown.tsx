a874358777954833a69bcd666d85485bab27b58f
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-08-27 23:21:12 +0200

    -

--- src/functional components/ChooseCountryDropdown.tsx
+++ src/functional components/ChooseCountryDropdown.tsx
@@ -13,21 +13,8 @@ import { currentlyPlaying, upcomingMovies } from '../services/films'
 import { CurrentCountryContext } from '../contexts/context'
 
 const ChooseCountry: React.FC = () => {
-  const { currentCountryKey, setCurrentCountryKey } = useContext(CurrentCountryContext)
-  const { setCurrentCountry } = useContext(CurrentCountryContext)
-  const { setFilms } = useContext(CurrentCountryContext)
-  const { setUpcomingFilms } = useContext(CurrentCountryContext)
-  const { currentCountry } = useContext(CurrentCountryContext)
-
-  console.log(currentCountry)
-
-  //on a specific element, the HTMLSelectElement
-  const changeCountry = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    const switchCountryKey = event.target.value
-    const countryToSwitch = countryOptions.find((country) => country.id === switchCountryKey)
-    setCurrentCountry(countryToSwitch!.label)
-    setCurrentCountryKey(countryToSwitch!.id)
-  }
+  const { currentCountryKey, setCurrentCountryKey, setCurrentCountry, setFilms, setUpcomingFilms } =
+    useContext(CurrentCountryContext)
 
   // Everytime the country changes change the currently playing
   useEffect(() => {
@@ -41,14 +28,22 @@ const ChooseCountry: React.FC = () => {
 
   // //Everytime the country key changes store the key in local storage
   useEffect(() => {
-    localStorage.setItem('current-country-key', JSON.stringify(currentCountryKey))
+    sessionStorage.setItem('current-country-key', currentCountryKey)
   }, [currentCountryKey])
+
+  //on a specific element, the HTMLSelectElement
+  const changeCountry = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const switchCountryKey = event.target.value
+    const countryToSwitch = countryOptions.find((country) => country.id === switchCountryKey)
+    setCurrentCountry(countryToSwitch!.label)
+    setCurrentCountryKey(countryToSwitch!.id)
+  }
 
   return (
     <>
       <CountryDropdown onChange={changeCountry}>
         <option selected={true} disabled={true}>
-          Choose your country
+          Country
         </option>
         {countryOptions.map((country) => (
           <option value={country.id}>{country.label}</option>
