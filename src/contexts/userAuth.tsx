7b50b57c718a31e39277a4e6fbb0386de820920c
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-08-27 23:21:16 +0200

    -

--- src/contexts/userAuth.tsx
+++ src/contexts/userAuth.tsx
@@ -1,6 +1,7 @@
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { CurrentCountryContext } from './context'
 
 export interface AuthRouteProps {
   children: React.ReactNode
@@ -11,6 +12,14 @@ const AuthRoute: React.FC<AuthRouteProps> = (props) => {
   const auth = getAuth()
   const navigate = useNavigate()
   const [loading, setLoading] = useState(false)
+  const { currentUID, setCurrentUID } = useContext(CurrentCountryContext)
+
+  //user login check
+  // onAuthStateChanged(auth, (user) => {
+  //   if (user) {
+  //     console.log(user.uid)
+  //   }
+  // })
 
   useEffect(() => {
     AuthCheck()
@@ -20,12 +29,11 @@ const AuthRoute: React.FC<AuthRouteProps> = (props) => {
   const AuthCheck = onAuthStateChanged(auth, (user) => {
     if (user) {
       setLoading(false)
-    } else {
+    } else if (!user) {
       console.log('Unauthorized')
       navigate('/signIn')
     }
   })
-
   if (loading) return <p>Loading...</p>
 
   return <>{children}</>
