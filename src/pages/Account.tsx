7828bc29dde6ae2694a4818d3ff22a62faee2bb3
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-08-28 15:16:07 +0200

    Remove from watchlist functionality movedinto a single file

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -1,25 +1,23 @@
-import { doc, getDoc } from 'firebase/firestore'
 import { useContext } from 'react'
-import { CurrentCountryContext } from '../contexts/context'
+import { MainStore } from '../contexts/context'
 import WatchlistCards from '../functional components/WatchlistCards'
-import { firestoreDB } from '../main'
 
 const Account = () => {
-  // const { userWatchList, setUserWatchList, signedInOrNot, currentUID } =
-  //   useContext(CurrentCountryContext)
-  //if user is logged in
-  //Then take the current UID and retrieve every film from the watchlist collection
-  //Then populate the usersWatchlist
-  //map out the cards to display the current users watchlist
-  // if (userDoc) {
-  //   console.log(userDoc)
-  // } else {
-  //   console.log('nothing found')
-  // }
+  const { userWatchList } = useContext(MainStore)
+
   return (
     <>
       <h2>Whats on your watchlist!</h2>
-      <WatchlistCards></WatchlistCards>
+      {userWatchList.map((film) => (
+        <WatchlistCards
+          title={film.original_title}
+          release_date={film.release_date}
+          rating={film.vote_average}
+          overview={film.overview}
+          poster_path={film.poster_path}
+          id={film.id}
+        ></WatchlistCards>
+      ))}
     </>
   )
 }
