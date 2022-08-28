b66a46c869f0edfcdb1c65e11ac2392777952460
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-08-28 15:21:26 +0200

    -

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,11 @@
 import ReactDOM from 'react-dom/client'
-import React, { useContext } from 'react'
+import React from 'react'
 import App from './pages/App'
 
 import { initializeApp } from 'firebase/app'
-import {
-  getFirestore,
-  collection,
-  getDocs,
-  addDoc,
-  deleteDoc,
-  doc,
-  getDoc,
-} from 'firebase/firestore'
-import { config } from './firebase'
-import { CurrentCountryContext } from './contexts/context'
+import { getFirestore, collection, query, getDocs } from 'firebase/firestore'
+import { config } from './firebase/firebaseConfig'
+import {} from './contexts/context'
 
 //Init firesbase app
 initializeApp(config.firebase)
@@ -21,32 +13,8 @@ initializeApp(config.firebase)
 //DB
 export const firestoreDB = getFirestore()
 
-//Collection
+//User collection so all you need to do is pass the doc/UID
 export const collectionReference = collection(firestoreDB, 'users')
-
-//get the collection data
-// getDocs(collectionReference).then((snapshot) => {
-//   let users = []
-
-//   snapshot.docs.forEach((doc) => {
-//     users.push({ ...doc.data(), id: doc.id })
-//   })
-
-//   console.log(users)
-// })
-
-// const docRef = doc(firestoreDB, 'users', currentUID)
-// const userDoc = await getDoc(docRef)
-
-// if (userDoc) {
-//   console.log(userDoc)
-// } else {
-//   console.log('nothing found')
-// }
-
-// const addDoc = (collectionReference, {} ) => {
-
-// }
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
