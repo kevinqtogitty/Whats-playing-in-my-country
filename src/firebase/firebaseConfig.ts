590ac6eaec6300153890e8aa95b15244586e642c
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-08-28 15:20:30 +0200

    -

---
+++ src/firebase/firebaseConfig.ts
@@ -0,0 +1,10 @@
+export const config = {
+  firebase: {
+    apiKey: 'AIzaSyCFX3OZqE09mTazvDsEJX9LkPeZyIMDjQY',
+    authDomain: 'whatsplayinginmycountry.firebaseapp.com',
+    projectId: 'whatsplayinginmycountry',
+    storageBucket: 'whatsplayinginmycountry.appspot.com',
+    messagingSenderId: '969679324976',
+    appId: '1:969679324976:web:ff9537bd69103117e72af5',
+  },
+}
