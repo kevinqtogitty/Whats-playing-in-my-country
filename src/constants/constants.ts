00241c4535e0d7c75c48aeab072a26739387a3e3
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-09-02 08:10:35 +0200

    -

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,13 +1,19 @@
 import { Countries } from '../types/interfaces_types'
 
-export const upcomingMoviesBaseUrl: String =
+export const movieBaseUrl: string = 'https://api.themoviedb.org/3/movie/'
+
+export const upcomingMoviesBaseUrl: string =
   'https://api.themoviedb.org/3/discover/movie?api_key=2b6cca6c77b9ad3c3ed56ae58ea9112d&primary_release_date.gte=2022-11-01&sort_by=release_date.asc&region='
 
-export const currentBaseUrl: String =
-  'https://api.themoviedb.org/3/movie/now_playing?api_key=2b6cca6c77b9ad3c3ed56ae58ea9112d&language=en-US&page=1&region='
+export const currentBaseUrl: string = `${movieBaseUrl}now_playing?api_key=2b6cca6c77b9ad3c3ed56ae58ea9112d&language=en-US&page=1&region=`
 
-export const posterBaseUrl: String = 'https://image.tmdb.org/t/p/w300'
+export const trailerUrl: string = `videos?api_key=2b6cca6c77b9ad3c3ed56ae58ea9112d`
 
+export const posterBaseUrl: string = 'https://image.tmdb.org/t/p/w300'
+
+export const youTubeTrailerBaseUrl: string = 'https://www.youtube.com/watch?v='
+
+export const youTubeEmbed: string = 'https://www.youtube.com/embed/'
 export const countryOptions: Countries[] = [
   { label: 'United Kingdom', id: 'GB' },
   { label: 'United States', id: 'US' },
