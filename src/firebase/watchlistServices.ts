dba387d7edac25170438b34bc00c3f12c972b78e
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-09-04 13:07:34 +0200

    -

--- src/firebase/watchlistServices.ts
+++ src/firebase/watchlistServices.ts
@@ -4,34 +4,34 @@ import { posterBaseUrl } from '../constants/constants'
 import { collectionReference } from '../main'
 import { Films, WatchlistProps } from '../types/interfaces_types'
 
-const addToWatchList = async (props: WatchlistProps) => {
+const addToWatchList = async (props: WatchlistProps): Promise<Films> => {
   const newWatchListFilm = {
     original_title: props.original_title,
     poster_path: `${posterBaseUrl}${props.poster_path}`,
     release_date: props.release_date,
     vote_average: props.vote_average,
     overview: props.overview,
-    id: props.id,
+    id: props.id
   }
 
   return newWatchListFilm
 }
 
-const addWatchListInDB = async (currentUID: string, newWatchListFilm: Films) => {
+const addWatchListInDB = async (currentUID: string, newWatchListFilm: Films): Promise<void> => {
   const userRef = doc(collectionReference, currentUID)
-  console.log(newWatchListFilm)
   await updateDoc(userRef, {
-    watchList: arrayUnion(newWatchListFilm),
+    watchList: arrayUnion(newWatchListFilm)
   })
-  return
 }
 
-const removeWatchListInDB = async (userWatchListMinusFilm: Films[], currentUID: string) => {
+const removeWatchListInDB = async (
+  userWatchListMinusFilm: Films[],
+  currentUID: string
+): Promise<void> => {
   const userRef = doc(collectionReference, currentUID)
   await updateDoc(userRef, {
-    watchList: userWatchListMinusFilm,
+    watchList: userWatchListMinusFilm
   })
-  return
 }
 
 export { addToWatchList, addWatchListInDB, removeWatchListInDB }
