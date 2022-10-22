496c335e94002e807c6cf05532ab66f8c5509d33
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-10-22 22:09:47 +0200

    styled components/interface pulled into seperate file

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,117 +1,24 @@
-import React, { MouseEventHandler, useContext } from 'react'
+import React, { useContext } from 'react'
 import Modal from 'react-modal'
-import styled from 'styled-components'
 
 import { MainStore } from '../contexts/context'
-import { Films } from '../types/interfaces_types'
+import { ModalProps } from '../types/interfaces_types'
 
 import { posterBaseUrl } from '../constants/constants'
-import { FilmPosters, Information } from './cards/WatchlistCards'
-import { CloseButton } from './re-usables/buttons'
+import { CloseButton } from '../stylesheets/styled_components/buttons'
+import { FilmPosters } from '../stylesheets/styled_components/currentFilmCardStyles'
+import {
+  CloseButtonWrapper,
+  FlexWrapper,
+  FlexWrapper3,
+  FlexWrapper2,
+  TrailerWrapper,
+  ReviewWrapper,
+  ReviewCard
+} from '../stylesheets/styled_components/modalStyles'
+import { Information } from '../stylesheets/styled_components/watchListCardsStyles'
 
-interface Props {
-  modalIsOpen: boolean
-  youtubeTrailers: string[]
-  toggleModal: MouseEventHandler
-  props: Films
-  availableOn: string[]
-  rentOn: string[]
-  reviews: any[]
-  cast: any[]
-  director: []
-  key?: number
-}
-
-const FlexWrapper = styled.div`
-  padding: 10px;
-  margin-top: -4rem;
-`
-const FlexWrapper2 = styled.div`
-  margin-top: 1rem;
-  margin-bottom: 2rem;
-  width: 28rem;
-  @media (max-width: 500px) {
-    margin-left: 0px;
-    padding: 0;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 100%;
-    text-align: center;
-  }
-
-  &.info {
-    margin-left: 2rem;
-    padding: 1rem;
-    width: 85%;
-    @media (max-width: 500px) {
-      margin-left: 0px;
-      padding: 0;
-      width: 100%;
-    }
-  }
-`
-
-const FlexWrapper3 = styled.div`
-  display: flex;
-  width: 100%;
-  padding: 0px;
-  @media (max-width: 500px) {
-    flex-wrap: wrap;
-  }
-
-  &.review_trailer {
-    column-gap: 2rem;
-  }
-`
-
-const CloseButtonWrapper = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  margin-bottom: 10px;
-`
-
-const TrailerWrapper = styled.div`
-  height: 40rem;
-  width: 100%;
-  overflow: scroll;
-  margin-left: 1rem;
-  margin-right: 1rem;
-  @media (max-width: 500px) {
-    height: 10rem;
-    margin-left: 0px;
-    margin-right: 0px;
-  }
-`
-
-const ReviewWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-  height: 30rem;
-  width: 100%;
-  flex-wrap: nowrap;
-  overflow: scroll;
-  ::-webkit-scrollbar {
-    display: none;
-  }
-`
-const ReviewCard = styled.section`
-  min-width: 25rem;
-  overflow: scroll;
-  margin: 2rem;
-  padding: 1rem;
-  font-size: 1em;
-  font-family: Raleway, sans-serif;
-  color: #f8f8f9;
-  background: #090506;
-  background: linear-gradient(to bottom, #090506, #070709);
-  box-shadow: 0 15px 30px rgba(0, 0, 0, 0.9);
-  @media (max-width: 500px) {
-    min-width: 15rem;
-    font-size: 0.8em;
-  }
-`
-const CardModal: React.FC<Props> = ({
+const CardModal: React.FC<ModalProps> = ({
   modalIsOpen,
   toggleModal,
   youtubeTrailers,
@@ -121,7 +28,7 @@ const CardModal: React.FC<Props> = ({
   reviews,
   director,
   cast
-}) => {
+}): JSX.Element => {
   const { currentCountry } = useContext(MainStore)
   return (
     <>
