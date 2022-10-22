4bc139529aeff8cc9a7fb93694ab9952e0db77cc
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-10-22 22:09:25 +0200

    styled components pulled into seperate file

--- src/components/UpcomingIn.tsx
+++ src/components/UpcomingIn.tsx
@@ -1,41 +1,20 @@
 import React, { useContext } from 'react'
-
 import { MainStore } from '../contexts/context'
-import styled from 'styled-components'
 import UpcomingFilmCards from './cards/UpcomingFIlmsCards'
+import {
+  FilmsBanner,
+  BannerHeader,
+  FilmCardWrapper
+} from '../stylesheets/styled_components/currentlyPlaying'
 
-export const BannerHeader = styled.div`
-  color: antiquewhite;
-  padding-left: 1.2rem;
-  position: absolute;
-  width: fit-content;
-`
-export const FilmsBanner = styled.div`
-  background-color: #000;
-  width: auto;
-  overflow: scroll;
-  padding: 1rem;
-  margin-top: -1.7rem;
-  margin: 0px;
-  ::-webkit-scrollbar {
-    display: none;
-  }
-`
-const FilmCardWrapper = styled.div`
-  display: flex;
-  column-gap: 1rem;
-  margin-top: 6rem;
-  @media (max-width: 500px) {
-    margin-top: 8rem;
-  }
-`
-
-const UpcomingIn: React.FC = () => {
+const UpcomingIn: React.FC = (): JSX.Element => {
   const { currentCountry, upcomingFilms, userWatchList, showTheMessage, showAddedMessage } =
     useContext(MainStore)
+
   const filteredFilms = upcomingFilms.filter(
     (film) => !userWatchList.some((userFilm) => film.id === userFilm.id)
   )
+
   return (
     <>
       <FilmsBanner>
