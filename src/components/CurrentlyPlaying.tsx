ad4984d5621c7655637f4dcf7b9ed8c8a2578ee1
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-10-22 22:09:56 +0200

    styled components/interface pulled into seperate file

--- src/components/CurrentlyPlaying.tsx
+++ src/components/CurrentlyPlaying.tsx
@@ -1,54 +1,14 @@
 import React, { useContext, useState } from 'react'
-import styled from 'styled-components'
 import { MainStore } from '../contexts/context'
+import { Button } from '../stylesheets/styled_components/buttons'
+import {
+  BannerHeader,
+  FilmCardWrapper,
+  FilmsBanner
+} from '../stylesheets/styled_components/currentlyPlaying'
 import CurrentFilmCards from './cards/CurrentFilmCards'
 
-export const BannerHeader = styled.div`
-  top: 65px;
-  color: antiquewhite;
-  padding-left: 1.2rem 0px 1.2rem 0px;
-  align-self: flex-start;
-  position: absolute;
-  width: 97%;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`
-export const FilmsBanner = styled.div`
-  background-color: #000;
-  width: auto;
-  overflow: scroll;
-  padding: 1rem;
-  margin-top: -1.7rem;
-  margin: 0px;
-  ::-webkit-scrollbar {
-    display: none;
-  }
-`
-const FilmCardWrapper = styled.div`
-  display: flex;
-  column-gap: 1rem;
-  margin-top: 6rem;
-  @media (max-width: 500px) {
-    margin-top: 8rem;
-  }
-`
-const Button = styled.button`
-  margin: 2em;
-  padding: 1em;
-  border: 1px solid #e4e6e8;
-  border-radius: 5px;
-  cursor: pointer;
-  transition: 0.1s ease-in;
-  &:hover {
-    background-color: forestgreen;
-    border: 1px solid forestgreen;
-
-    color: white;
-  }
-`
-
-const CurrrentlyPlaying: React.FC = () => {
+const CurrrentlyPlaying: React.FC = (): JSX.Element => {
   const [ascOrder, setOrder] = useState(false)
   const { currentCountry, films, userWatchList, showAddedMessage, showTheMessage, setFilms } =
     useContext(MainStore)
