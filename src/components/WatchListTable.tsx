2ac7a850f8f7d39bf7326033e7142a8cba73e2e7
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-10-22 22:09:19 +0200

    styled components pulled into seperate file

--- src/components/WatchListTable.tsx
+++ src/components/WatchListTable.tsx
@@ -1,74 +1,17 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { removeWatchListInDB } from '../firebase/watchlistServices'
-import styled from 'styled-components'
 
 import { getAvailableOn, getCastAndCrew, getReviews, getTrailer } from '../services/films'
 import { youTubeEmbed } from '../constants/constants'
 import { MainStore } from '../contexts/context'
 import CardModal from './Modal'
 
-import { Trailer } from '../types/interfaces_types'
 import removeSvg from '../assets/img/trash.svg'
+import { TableProps, Trailer } from '../types/interfaces_types'
+import { TrashIcon } from '../stylesheets/styled_components/watchListCardsStyles'
+import { Cell, ListItem } from '../stylesheets/styled_components/watchListTableStyles'
 
-const ListItem = styled.li`
-  background-color: #ffffff;
-  box-shadow: 0px 0px 9px 0px rgba(0, 0, 0, 0.1);
-  list-style-type: none;
-  display: flex;
-  cursor: pointer;
-`
-const Cell = styled.span`
-  text-decoration: none;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 10px 0;
-  height: 3rem;
-
-  &.title {
-    width: 18rem;
-    padding-left: 5px;
-    text-align: center;
-  }
-
-  &.releaseDate {
-    width: 21.5rem;
-    @media (max-width: 500px) {
-      display: none;
-    }
-  }
-
-  &.rating {
-    width: 10.3rem;
-    justify-content: flex-end;
-  }
-
-  &.remove {
-    width: 8rem;
-    justify-content: flex-end;
-    padding-right: 5px;
-  }
-`
-
-const TrashIcon = styled.img`
-  width: 2rem;
-  margin-top: -12rem;
-  cursor: pointer;
-  height: fit-content;
-  margin-top: 1px;
-`
-
-interface TableProps {
-  original_title: string
-  release_date: string
-  rating: number
-  id: number
-  overview: string
-  poster_path: string
-  vote_average: number
-}
-
-const WatchListTableRows: React.FC<TableProps> = (props) => {
+const WatchListTableRows: React.FC<TableProps> = (props): JSX.Element => {
   const [modalIsOpen, setModalIsOpen] = useState<boolean>(false)
   const [trailer, setTrailer] = useState<Trailer[]>([])
   const [availableOn, setAvailableOn] = useState<string[]>([])
